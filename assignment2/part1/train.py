3eb539c6822422b9322ec498d06cd0c92160209a
Sergei <mailbox.sa02@gmail.com>
2022-11-30 23:02:06 +0100

    Assignment 2 part1.2

--- assignment2/part1/train.py
+++ assignment2/part1/train.py
@@ -21,9 +21,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.data as data
 import torchvision.models as models
+import os
 
+from tqdm.auto import tqdm
 from cifar100_utils import get_train_validation_set, get_test_set
 
+MODELS_PATH = "./saved_models"
 
 def set_seed(seed):
     """
@@ -52,10 +55,17 @@ def get_model(num_classes=100):
     #######################
 
     # Get the pretrained ResNet18 model on ImageNet from torchvision.models
-    pass
+    model = models.resnet18(weights='IMAGENET1K_V1')
+    # Freeze all layers
+    for param in model.features.parameters():
+        param.requires_grad = False
 
     # Randomly initialize and modify the model's last layer for CIFAR100.
-    pass
+    in_features = model.fc.in_features
+    model.fc = torch.nn.Linear(in_features, num_classes)  # has requires_grad = True on initialization
+
+    nn.init.normal_(model.fc.weight, mean=0.0, std=0.01)
+    nn.init.zeros_(model.fc.bias)
 
     #######################
     # END OF YOUR CODE    #
@@ -85,16 +95,62 @@ def train_model(model, lr, batch_size, epochs, data_dir, checkpoint_name, device
     #######################
 
     # Load the datasets
-    pass
+    train_set, val_set = get_train_validation_set(data_dir)
+
+    train_loader = data.DataLoader(train_set, batch_size, shuffle=True)
+    val_loader = data.DataLoader(val_set, batch_size, shuffle=True)
 
     # Initialize the optimizer (Adam) to train the last layer of the model.
-    pass
+    optimizer = torch.optim.Adam([p for p in model.parameters() if p.requires_grad == True], lr=lr) 
+    criterion = nn.CrossEntropyLoss()
 
     # Training loop with validation after each epoch. Save the best model.
-    pass
+    model.train() 
+    best_accuracy = -np.inf
+
+    for epoch in tqdm(range(epochs)):
+        epoch_loss = 0.0
+        for images, labels in train_loader:
+
+            # Move the training data on GPU
+            images = images.to(device)
+            labels = labels.to(device)
+
+            # Run the model on the input data
+            scores = model.forward(images)
+
+            # Calculate loss
+            current_loss = criterion(scores, labels)
+
+            # Perform backpropagation
+            optimizer.zero_grad()
+            current_loss.backward(retain_graph=True)
+
+            # Update the parameters
+            optimizer.step()
+            
+            # Take the running average of the loss
+            epoch_loss += current_loss.item()
+
+            # Clear computation graph
+            labels = labels.detach()
+            scores = scores.detach()
+
+        epoch_loss /= len(train_loader)
+
+        # Validation loop
+        val_accuracy = evaluate_model(model, val_loader, device)
+
+        if epoch == 0:
+            best_accuracy = val_accuracy
+            torch.save(model, checkpoint_name)
+        else:
+            if best_accuracy < val_accuracy:
+                best_accuracy = val_accuracy
+                torch.save(model, checkpoint_name)
 
     # Load the best model on val accuracy and return it.
-    pass
+    model = torch.load(checkpoint_name)
 
     #######################
     # END OF YOUR CODE    #
@@ -119,11 +175,33 @@ def evaluate_model(model, data_loader, device):
     # PUT YOUR CODE HERE  #
     #######################
     # Set model to evaluation mode (Remember to set it back to training mode in the training loop)
-    pass
+    model.eval()
+    correct, total = 0, 0
 
     # Loop over the dataset and compute the accuracy. Return the accuracy
     # Remember to use torch.no_grad().
-    pass
+    with torch.no_grad():
+        for images, labels in data_loader:
+
+            # Move the data on GPU
+            images = images.to(device)
+            labels = labels.to(device)
+
+            # Run the model on the input data
+            scores = model.forward(images)
+
+            # Clear the computational graph
+            images = images.detach().cpu()
+            labels = labels.detach().cpu()
+
+            # Get the predicted labels
+            predicted = torch.argmax(scores, dim=1)
+
+            # get all correct predictions
+            correct += (predicted == labels).sum().item()
+            total += labels.shape[0]
+    
+    accuracy = correct / total
 
     #######################
     # END OF YOUR CODE    #
@@ -148,22 +226,27 @@ def main(lr, batch_size, epochs, data_dir, seed, augmentation_name):
     # PUT YOUR CODE HERE  #
     #######################
     # Set the seed for reproducibility
-    pass
+    set_seed(seed)
 
     # Set the device to use for training
-    pass
+    device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 
     # Load the model
-    pass
+    model = get_model()
 
     # Get the augmentation to use
-    pass
+    
 
     # Train the model
-    pass
+    os.makedirs(MODELS_PATH, exist_ok=True)
+    checkpoint_name = f'{MODELS_PATH}/resnet18_lr{lr}_batchsize{batch_size}_augmentation_{augmentation_name}.model'
+    model = train_model(model, lr, batch_size, epochs, data_dir, checkpoint_name, device)
 
     # Evaluate the model on the test set
-    pass
+    test_set = get_test_set(data_dir)
+    test_loader = data.DataLoader(test_set, batch_size, shuffle=False)
+    accuracy = evaluate_model(model, test_loader, device)
+    print(f'FINAL PERFORMANCE:{accuracy:.3f}')
 
     #######################
     # END OF YOUR CODE    #
